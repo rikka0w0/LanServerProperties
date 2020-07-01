422330337cbc9959275b3e23b3a6638720011dd3
Rikka0w0 <929513338@qq.com>
2020-07-02 06:44:54 +1000

    1.12.2 backport

--- src/main/java/rikka/lanserverproperties/LanServerProperties.java
+++ src/main/java/rikka/lanserverproperties/LanServerProperties.java
@@ -1,16 +1,11 @@
 package rikka.lanserverproperties;
 
-import org.apache.commons.lang3.tuple.Pair;
-
-import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.client.event.GuiOpenEvent;
 import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.fml.DistExecutor;
-import net.minecraftforge.fml.ExtensionPoint;
-import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.network.FMLNetworkConstants;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
-@Mod(LanServerProperties.MODID)
+@Mod(modid = LanServerProperties.MODID, clientSideOnly = true)
 public class LanServerProperties {
 	public static final String MODID = "lanserverproperties";
 
@@ -21,34 +16,14 @@ public class LanServerProperties {
 			INSTANCE = this;
 		else
 			throw new RuntimeException("Duplicated Class Instantiation: CustomItems");
-
-		// Make sure the mod being absent on the other network side does not cause the client to display the server as incompatible
-		ModLoadingContext.get().registerExtensionPoint(ExtensionPoint.DISPLAYTEST, 
-				() -> Pair.of(() -> FMLNetworkConstants.IGNORESERVERONLY, (a, b) -> true)
-				);
 		
 		// Register the GuiOpenEvent handler on client side only
 		// If the mod is accidentally installed on a dedicated server then do nothing
-		DistExecutor.runWhenOn(Dist.CLIENT, 
-				()->()->MinecraftForge.EVENT_BUS.addListener(ShareToLanScreen2.guiOpenEventHandler)
-		);
+		MinecraftForge.EVENT_BUS.register(this);
 	}
 
-/*
-	@Mod.EventBusSubscriber(modid = LanServerProperties.MODID, bus = Mod.EventBusSubscriber.Bus.FORGE)
-	public static class ForgeEventHandler {
-		@SubscribeEvent
-		public static void onServerStarting(FMLServerStartingEvent e) {
-			MinecraftServer server = e.getServer();
-			Path path = Paths.get(FMLPaths.CONFIGDIR.get().toString(), "server.properties");
-			ServerPropertiesProvider serverpropertiesprovider = new ServerPropertiesProvider(path);
-			if (!Files.exists(path))
-				serverpropertiesprovider.save();
-
-			ServerProperties props = serverpropertiesprovider.getProperties();
-			server.setOnlineMode(props.onlineMode);
-			server.setServerPort(props.serverPort);
-		}
+	@SubscribeEvent
+	public void guiOpenEventHandler(GuiOpenEvent event) {
+		ShareToLanScreen2.guiOpenEventHandler.accept(event);
 	}
-*/
 }
--- src/main/java/rikka/lanserverproperties/ShareToLanScreen2.java
+++ src/main/java/rikka/lanserverproperties/ShareToLanScreen2.java
@@ -1,87 +1,99 @@
 package rikka.lanserverproperties;
 
-import java.lang.reflect.Field;
-import java.lang.reflect.Modifier;
+import java.io.IOException;
+import java.net.InetAddress;
 import java.util.function.Consumer;
 
 import net.minecraft.client.Minecraft;
-import net.minecraft.client.gui.screen.Screen;
-import net.minecraft.client.gui.screen.ShareToLanScreen;
-import net.minecraft.client.gui.widget.TextFieldWidget;
-import net.minecraft.client.gui.widget.Widget;
-import net.minecraft.client.gui.widget.button.Button;
+import net.minecraft.client.gui.GuiButton;
+import net.minecraft.client.gui.GuiTextField;
+import net.minecraft.client.gui.GuiScreen;
+import net.minecraft.client.gui.GuiShareToLan;
 import net.minecraft.client.resources.I18n;
 import net.minecraft.util.text.ITextComponent;
-import net.minecraft.util.text.TranslationTextComponent;
+import net.minecraft.util.text.TextComponentString;
+import net.minecraft.util.text.TextComponentTranslation;
 import net.minecraft.world.GameType;
-import net.minecraftforge.api.distmarker.Dist;
-import net.minecraftforge.api.distmarker.OnlyIn;
 import net.minecraftforge.client.event.GuiOpenEvent;
 import net.minecraftforge.fml.common.ObfuscationReflectionHelper;
+import net.minecraftforge.fml.relauncher.SideOnly;
+import net.minecraftforge.fml.relauncher.Side;
 
-@OnlyIn(Dist.CLIENT)
-public class ShareToLanScreen2 extends ShareToLanScreen {
+@SideOnly(Side.CLIENT)
+public class ShareToLanScreen2 extends GuiShareToLan {
 	public final static String onlineModeLangKey = "lanserverproperties.gui.online_mode";
 	public final static String onlinemodeDescLangKey = "lanserverproperties.gui.online_mode_desc";
 	public final static String portLangKey = "lanserverproperties.gui.port";
 	public final static Consumer<GuiOpenEvent> guiOpenEventHandler = (e)-> {
-		Screen guiToBeOpened = e.getGui();
-		if (guiToBeOpened instanceof ShareToLanScreen) {
+		GuiScreen guiToBeOpened = e.getGui();
+		if (guiToBeOpened instanceof GuiShareToLan) {
 /*
 			Screen lastScreen = ObfuscationReflectionHelper.getPrivateValue(
 					ShareToLanScreen.class, 
 					(ShareToLanScreen) guiToBeOpened, 
 					"field_146598_a");
 */
-			Screen lastScreen = Minecraft.getInstance().currentScreen;
+			GuiScreen lastScreen = Minecraft.getMinecraft().currentScreen;
 			e.setGui(new ShareToLanScreen2(lastScreen));
 		}
 	};
 	
-	protected final Screen lastScreen;
-	protected TextFieldWidget tfwPort = null;
-	protected Button onlineModeButton = null;
+	protected final GuiScreen lastScreen;
+	protected GuiTextField tfwPort = null;
+	protected GuiButton onlineModeButton = null;
 	protected boolean onlineMode = true;
-	
-	public ShareToLanScreen2(Screen lastScreenIn) {
+
+	public ShareToLanScreen2(GuiScreen lastScreenIn) {
 		super(lastScreenIn);
 		this.lastScreen = lastScreenIn;
 	}
+
+	@Override
+	protected void actionPerformed(GuiButton button) throws IOException {
+		if (button.id == 101) {
+			serverStartButtonClick(button);
+		} else if (button.id == 233) {
+			this.onlineMode = !this.onlineMode;
+			button.displayString = this.getOnlineButtonText();
+		} else { 
+			super.actionPerformed(button);
+		}
+	}
 	
-	Button.IPressable startServerButtonPressed = (button) -> {
-		this.minecraft.displayGuiScreen((Screen) null);
+	private void serverStartButtonClick(GuiButton button) throws IOException {
+		this.mc.displayGuiScreen(null);
 
 		String portStr = tfwPort.getText();
 		int port = portStr.length() > 0 ? Integer.parseInt(portStr) : 25565;
-		String gameMode = ObfuscationReflectionHelper.getPrivateValue(ShareToLanScreen.class, ShareToLanScreen2.this, "field_146599_h");
-		boolean allowCheats = ObfuscationReflectionHelper.getPrivateValue(ShareToLanScreen.class, ShareToLanScreen2.this, "field_146600_i");
+		String gameMode = ObfuscationReflectionHelper.getPrivateValue(GuiShareToLan.class, this, "field_146599_h");
+		boolean allowCheats = ObfuscationReflectionHelper.getPrivateValue(GuiShareToLan.class, this, "field_146600_i");
 
 		ITextComponent itextcomponent;
-		if (this.minecraft.getIntegratedServer().shareToLAN(GameType.getByName(gameMode), allowCheats, port)) {
-			itextcomponent = new TranslationTextComponent("commands.publish.started", port);
+		String newPort = this.mc.getIntegratedServer().shareToLAN(GameType.getByName(gameMode), allowCheats);
+		if (newPort != null) {
+			this.mc.getIntegratedServer().getNetworkSystem().addLanEndpoint((InetAddress)null, port);
+			itextcomponent = new TextComponentTranslation("commands.publish.started", newPort + ", " + port);
+			this.mc.getIntegratedServer().setOnlineMode(onlineMode);
 		} else {
-			itextcomponent = new TranslationTextComponent("commands.publish.failed");
+			itextcomponent = new TextComponentString("commands.publish.failed");
 		}
-		
-		this.minecraft.getIntegratedServer().setOnlineMode(onlineMode);
-		this.minecraft.getIntegratedServer().getMaxPlayers();
-		this.minecraft.ingameGUI.getChatGUI().printChatMessage(itextcomponent);
-	};
+		this.mc.ingameGUI.getChatGUI().printChatMessage(itextcomponent);
+	}
 	
 	@Override
-	protected void init() {
-		super.init();
+	public void initGui() {
+		super.initGui();
 
 		// Attempt to locate the old button
-		Button button = null;
+		GuiButton button = null;
 		String msg = I18n.format("lanServer.start");
-		for (Widget widget: this.buttons) {
-			if (widget instanceof Button && widget.getMessage().equals(msg)) {
-				button = (Button) widget;
+		for (GuiButton widget: this.buttonList) {
+			if (widget.displayString.equals(msg)) {
+				button = widget;
 				break;
 			}
 		}
-		
+
 		if (button == null) {
 			System.out.println("[LSP] Unable to locate start server button!");
 			// If we cannot find the "start lan server" button
@@ -89,53 +101,49 @@ public class ShareToLanScreen2 extends ShareToLanScreen {
 			return;
 		}
 
-		Field fieldOnPress = ObfuscationReflectionHelper.findField(Button.class, "onPress");
-		Field modifiersField;
-		try {
-			modifiersField = Field.class.getDeclaredField("modifiers");
-			modifiersField.setAccessible(true);
-			modifiersField.setInt(fieldOnPress, fieldOnPress.getModifiers() & ~Modifier.FINAL & ~Modifier.PRIVATE);
-			modifiersField.setAccessible(false);
-			
-			fieldOnPress.set(button, startServerButtonPressed);
-		} catch (NoSuchFieldException | SecurityException | IllegalArgumentException | IllegalAccessException e) {
-			e.printStackTrace();
-		}
-
 		// Add our own widgets
 		// Toggle button for onlineMode
 		this.onlineModeButton = 
-				new Button(this.width / 2 - 155, 124, 150, 20, getOnlineButtonText(),
-				(p_214315_1_) -> {
-					this.onlineMode = !this.onlineMode;
-					this.onlineModeButton.setMessage(this.getOnlineButtonText());
-				});
+				new GuiButton(233, this.width / 2 - 155, 124, 150, 20, getOnlineButtonText());
 		this.addButton(this.onlineModeButton);
 
 		// Text field for port
-		this.tfwPort = new TextFieldWidget(this.font, this.width / 2 - 154, this.height - 54, 147, 20, I18n.format(portLangKey));
+		this.tfwPort = new GuiTextField(234, this.fontRenderer, this.width / 2 - 154, this.height - 54, 147, 20);
 		this.tfwPort.setText("25565");
-		// Check the format, make sure the text is a valid integer
-		this.tfwPort.func_212954_a((text)->this.tfwPort.setTextColor(validatePort(text) >= 0 ? 0xFFFFFF : 0xFF0000));
-		this.children.add(tfwPort);
 	}
 	
-	@Override
-	public void tick() {
-		this.tfwPort.tick();
-	}
-	
-	@Override
-	public void render(int mouseX, int mouseY, float partialTicks) {
-		super.render(mouseX, mouseY, partialTicks);
+//	@Override
+//	public void tick() {
+//		this.tfwPort.tick();
+//	}
 
-		this.drawString(this.font, I18n.format(portLangKey), this.width / 2 - 154, this.height - 66, 10526880);
-		this.tfwPort.render(mouseX, mouseY, partialTicks);
-
-		if (this.onlineModeButton.isHovered())
-			this.renderTooltip(I18n.format(onlinemodeDescLangKey), mouseX, mouseY);
+	@Override
+	protected void keyTyped(char typedChar, int keyCode) throws IOException {
+		if (this.tfwPort.textboxKeyTyped(typedChar, keyCode)) {
+			// Check the format, make sure the text is a valid integer
+			this.tfwPort.setTextColor(validatePort(this.tfwPort.getText()) >= 0 ? 0xFFFFFF : 0xFF0000);
+		} else {
+			super.keyTyped(typedChar, keyCode);
+		}
 	}
-	
+
+	@Override
+	protected void mouseClicked(int mouseX, int mouseY, int mouseButton) throws IOException {
+		super.mouseClicked(mouseX, mouseY, mouseButton);
+		this.tfwPort.mouseClicked(mouseX, mouseY, mouseButton);
+	}
+
+	@Override
+	public void drawScreen(int mouseX, int mouseY, float partialTicks) {
+		super.drawScreen(mouseX, mouseY, partialTicks);
+
+		this.drawString(this.fontRenderer, I18n.format(portLangKey), this.width / 2 - 154, this.height - 66, 10526880);
+		this.tfwPort.drawTextBox();
+
+		if (this.onlineModeButton.isMouseOver())
+			this.drawHoveringText(I18n.format(onlinemodeDescLangKey), mouseX, mouseY);
+	}
+
 	/**
 	 * @param text
 	 * @return negative if port is invalid, otherwise the port number
