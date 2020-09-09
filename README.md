0d64f17af011a1450a5c5dbc0d140a28a432267f
Rikka0w0 <929513338@qq.com>
2020-09-10 05:41:53 +1000

    Update to v1.4, Forge 1.16.2

--- README.md
+++ README.md
@@ -1,12 +1,25 @@
 # LAN Server Properties
-For Minecraft 1.12–1.16, and Forge.
+For Minecraft 1.12–1.16, Forge and Fabric.
 
 When this mod is installed, it enhances the vanilla Minecraft "Open to LAN" screen, which now also:
 * Allows for a port customization
 * Allows a user to disable the online mode, so that also unauthenticated players can join the LAN server.
+* This mod is client only, installing on a dedicated server will not affact anything.
+* Only the Lan server owner need to install this mod.
+* Installing this mod does not prevent you from joining other vanilla or mod servers.
+
+##Dependencies
+### Forge Version
+You need to install Forge and then install this mod.
+### 
+You need to install Fabric Loader and then install this mod. Fabric API is optional.
 
 ## For developers
-To modify and debug the code, first import the repo as a Gradle project in Eclipse IDE, and then run the ForgeGradle task `genEclipseRuns`.
+To modify and debug the code, first import the repo as a Gradle project in Eclipse IDE, and then run the gradle task `genEclipseRuns`.
+Most design work should happen in Fabric using yarn mapping. However, after remapping (see the following section), the forge folder can be imported to IDE as well.
+If the forge part cannot be imported into Eclipse, please copy `gradlew`, `gradlew.bat` and `gradle` folder into `forge` folder and retry.
+
+Windows users need to replace `./` and `../` with `.\` and `..\` respectively.
 
 ### Compile Fabric artifact
 ```
@@ -19,10 +32,20 @@ cd LanServerProperties
 ```
 git clone git@github.com:rikka0w0/yarnforge-plugin.git
 cd LanServerProperties
+git submodule update --init
+
+# Create vanilla-to-MCP tinyV2 mapping for Fabric Loom
 pushd forge
+# This will fail with `java.nio.file.NoSuchFileException`, it is normal.
 ../gradlew userRemapYarn --mappings net.fabricmc:yarn:1.16.2+build.47 --mc-version 1.16.2 --no-daemon --stacktrace --debug
+../gradlew packMapping
 popd
+
+# Fabric Loom migrateMappings
 ./gradlew migrateMappings --mappings rikka:obf2mcp --input src/main/java --output forge/src/main/java
+# Mapping to MCP is done
+
+# Forge gradle build
 pushd forge
 ../gradlew build
 popd
--- src/forge/java/rikka/lanserverproperties/LanServerProperties.java
+++ src/forge/java/rikka/lanserverproperties/LanServerProperties.java
@@ -1,13 +1,28 @@
 package rikka.lanserverproperties;
 
+import java.lang.reflect.Field;
+import java.lang.reflect.Modifier;
+
 import org.apache.commons.lang3.tuple.Pair;
 
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.FontRenderer;
+import net.minecraft.client.gui.screen.Screen;
+import net.minecraft.client.gui.screen.ShareToLanScreen;
+import net.minecraft.client.gui.widget.Widget;
+import net.minecraft.client.gui.widget.button.Button;
+import net.minecraft.client.resources.I18n;
+import net.minecraft.util.text.ITextComponent;
+import net.minecraft.util.text.TranslationTextComponent;
+import net.minecraft.world.GameType;
 import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.client.event.GuiScreenEvent;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.DistExecutor;
 import net.minecraftforge.fml.ExtensionPoint;
 import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.ObfuscationReflectionHelper;
 import net.minecraftforge.fml.network.FMLNetworkConstants;
 
 @Mod(LanServerProperties.MODID)
@@ -32,27 +47,93 @@ public class LanServerProperties {
 		DistExecutor.safeRunWhenOn(Dist.CLIENT, ()->ClientHandler::registerGuiEventHandler);
 	}
 
-	public static class ClientHandler {
+	private static class ClientHandler {
 		public static void registerGuiEventHandler() {
-			MinecraftForge.EVENT_BUS.addListener(ShareToLanScreen2::guiOpenEventHandler);
+			MinecraftForge.EVENT_BUS.addListener(ClientHandler::onGuiInit);
+			MinecraftForge.EVENT_BUS.addListener(ClientHandler::onGuiDraw);
 		}
-	}
-	
-/*
-	@Mod.EventBusSubscriber(modid = LanServerProperties.MODID, bus = Mod.EventBusSubscriber.Bus.FORGE)
-	public static class ForgeEventHandler {
-		@SubscribeEvent
-		public static void onServerStarting(FMLServerStartingEvent e) {
-			MinecraftServer server = e.getServer();
-			Path path = Paths.get(FMLPaths.CONFIGDIR.get().toString(), "server.properties");
-			ServerPropertiesProvider serverpropertiesprovider = new ServerPropertiesProvider(path);
-			if (!Files.exists(path))
-				serverpropertiesprovider.save();
 
-			ServerProperties props = serverpropertiesprovider.getProperties();
-			server.setOnlineMode(props.onlineMode);
-			server.setServerPort(props.serverPort);
+		public static void onGuiInit(GuiScreenEvent.InitGuiEvent.Post event) {
+			Screen gui = event.getGui();
+			if (!(gui instanceof ShareToLanScreen))
+				return;
+
+			// Attempt to locate the old button
+			Button button = null;
+			String msg = I18n.format("lanServer.start");
+			for (Widget widget: event.getWidgetList()) {
+				if (widget instanceof Button && widget.func_230458_i_().getString().equals(msg)) {
+					button = (Button) widget;
+					break;
+				}
+			}
+
+			if (button == null) {
+				System.out.println("[LSP] Unable to locate start server button!");
+				// If we cannot find the "start lan server" button
+				// just leave everything else there
+				return;
+			}
+
+			// Replace the vanilla button click handler
+			// Todo: replace this with a Mixin for better compatibility with other mods
+			Field fieldOnPress = ObfuscationReflectionHelper.findField(Button.class, "field_230697_t_");
+			Field modifiersField;
+			try {
+				modifiersField = Field.class.getDeclaredField("modifiers");
+				modifiersField.setAccessible(true);
+				modifiersField.setInt(fieldOnPress, fieldOnPress.getModifiers() & ~Modifier.FINAL & ~Modifier.PRIVATE);
+				modifiersField.setAccessible(false);
+				
+				fieldOnPress.set(button, new LanServerStartButtonHandler((ShareToLanScreen) gui));
+			} catch (NoSuchFieldException | SecurityException | IllegalArgumentException | IllegalAccessException e) {
+				e.printStackTrace();
+			}
+
+			FontRenderer textRenderer = ObfuscationReflectionHelper.getPrivateValue(Screen.class, gui, "field_230712_o_");
+			OpenToLanScreenEx.init(gui, textRenderer, event.getWidgetList(), event::addWidget);
+		}
+
+		public static void onGuiDraw(GuiScreenEvent.DrawScreenEvent.Post event) {
+			Screen gui = event.getGui();
+			if (!(gui instanceof ShareToLanScreen))
+				return;
+
+			FontRenderer textRenderer = ObfuscationReflectionHelper.getPrivateValue(Screen.class, gui, "field_230712_o_");
+			OpenToLanScreenEx.postDraw(gui, textRenderer, event.getMatrixStack(),
+					event.getMouseX(), event.getMouseY(), event.getRenderPartialTicks());
 		}
 	}
-*/
+
+	/**
+	 * Logic copied from vanilla {@link SahreToLanScreen}.
+	 */
+	private static class LanServerStartButtonHandler implements Button.IPressable {
+		public final ShareToLanScreen owner;
+
+		private LanServerStartButtonHandler(ShareToLanScreen owner) {
+			this.owner = owner;
+		}
+
+		@Override
+		public void onPress(Button theButton) {
+			Minecraft mc = Minecraft.getInstance();
+			mc.displayGuiScreen((Screen) null);
+
+			int port = OpenToLanScreenEx.getServerPort(owner);
+			String gameMode = ObfuscationReflectionHelper.getPrivateValue(ShareToLanScreen.class, owner, "field_146599_h");
+			boolean allowCheats = ObfuscationReflectionHelper.getPrivateValue(ShareToLanScreen.class, owner, "field_146600_i");
+
+			ITextComponent itextcomponent;
+			if (mc.getIntegratedServer().shareToLAN(GameType.getByName(gameMode), allowCheats, port)) {
+				OpenToLanScreenEx.onOpenToLanSuccess(owner);
+				itextcomponent = new TranslationTextComponent("commands.publish.started", port);
+			} else {
+				itextcomponent = new TranslationTextComponent("commands.publish.failed");
+			}
+
+			mc.ingameGUI.getChatGUI().printChatMessage(itextcomponent);
+			mc.func_230150_b_(); // Update window title
+		}
+	}
 }
